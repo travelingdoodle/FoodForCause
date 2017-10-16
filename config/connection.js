0ab03853acc15ee59b52f511443edd5bc6ef6aca
travelingdoodle <reggiebrown@gmail.com>
2017-10-16 16:40:39 -0500

    JawsDB added to connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,4 +10,24 @@ var sequelize = new Sequelize("food_db", "root", "password", {
   }
 });
 
+// Added according to heroku deployment guide
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'food_db'
+
+  });
+};
+connection.connect();
+module.exports = connection;
+
+// export sequelize
 module.exports = sequelize;
+
