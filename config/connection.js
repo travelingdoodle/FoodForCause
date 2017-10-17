36cc241802710e0444f31077b467425dc59f9560
travelingdoodle <reggiebrown@gmail.com>
2017-10-17 00:12:19 -0500

    cleared index.js / updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,15 @@
 
 // 3-13 works locally
 // However, i don't think that we need this anymore. so commented out for now.
-var sequelize = new Sequelize("food_db", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// var sequelize = new Sequelize("food_db", "root", "password", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
 // Added according to heroku deployment guide (14-30)
 // var mysql = require('mysql');
@@ -53,3 +53,27 @@ var sequelize = new Sequelize("food_db", "root", "password", {
 // remvoing because it would be duplicated based on index.js
 //module.exports = sequelize;
 
+// copied from index.js to maintain current file structure
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+//this is what it looks like in connection.js
+module.exports= sequelize;
+// not sure we need this one, Including it for now
+//module.exports = db;
