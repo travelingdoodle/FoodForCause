727470cbec78d6c7bed4db3b543b18839eaedb47
travelingdoodle <reggiebrown@gmail.com>
2017-10-17 00:02:38 -0500

    created config.json and index.js commentedout connection.js should work for sequelize on jawsdb heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-var Sequelize = require("sequelize");
+// var Sequelize = require("sequelize");
 
-//4-12 works locally
-// var sequelize = new Sequelize("food_db", "root", "password", {
-//   host: "localhost",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
+// 3-13 works locally
+// However, i don't think that we need this anymore. so commented out for now.
+var sequelize = new Sequelize("food_db", "root", "password", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
 // Added according to heroku deployment guide (14-30)
 // var mysql = require('mysql');
@@ -29,24 +30,26 @@ var Sequelize = require("sequelize");
 // connection.connect();
 // module.exports = connection;
 
-//bastardized trial version to make sequelize work on heroku 32 - 55
+//bastardized trial version to make sequelize work on heroku 32 - 50
 
-// This block is from the sequelize heroku guide
-if (process.env.JAWSDB_URL) {
-  // the application is executed on Heroku ... use the msql database
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-  // the application is executed on the local machine ... use mysql
-  sequelize = new Sequelize('food_db', 'root', "password", {
-    host: "localhost",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-  });
-}
+    // // This block is from the sequelize heroku guide
+    // if (process.env.JAWSDB_URL) {
+    //   // the application is executed on Heroku ... use the msql database
+    //   sequelize = new Sequelize(process.env.JAWSDB_URL);
+    //   } else {
+    //   // the application is executed on the local machine ... use mysql
+    //   sequelize = new Sequelize('food_db', 'root', "password", {
+    //     host: "localhost",
+    //     dialect: "mysql",
+    //     pool: {
+    //       max: 5,
+    //       min: 0,
+    //       idle: 10000
+    //     }
+    //   });
+    // }
+
 // export sequelize
-module.exports = sequelize;
+// remvoing because it would be duplicated based on index.js
+//module.exports = sequelize;
 
---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+//this is what it looks like in connection.js
+module.exports= sequelize;
+// not sure we need this one, Including it for now
+module.exports = db;
