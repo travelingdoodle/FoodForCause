fae4c8c4d055fa6cca637f8cf0c9fa0037f03851
travelingdoodle <reggiebrown@gmail.com>
2017-10-21 00:21:57 -0500

    updated readme and package.json

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-// Dependencies needed for Passport.js GitHub OAuth
-// var passport = require('passport');
-// var util = require('util');
-// var session = require('express-session');
-// var methodOverride = require('method-override');
-// var GitHubStrategy = require('passport-github2').Strategy;
-// var partials = require('express-partials');
-
-// var GITHUB_CLIENT_ID = "";
-// var GITHUB_CLIENT_SECRET = "";
-
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -20,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
