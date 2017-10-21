76f6dfc075f8e106f53b2ec3f4610ed4ad2e57b1
travelingdoodle <reggiebrown@gmail.com>
2017-10-21 00:59:48 -0500

    sync sequelize server.js

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
 // new comment
+sequelize.sync({force: true}).then(function(){
+	app.listen(PORT, function(){
+	console.log("listenning on http://localhost:" + PORT);
+});
+});
