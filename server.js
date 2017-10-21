31043a4c315e5d672acdaeeff1ad0fd1a29bb54a
travelingdoodle <reggiebrown@gmail.com>
2017-10-21 01:40:44 -0500

    nothing works because we need to refactor and other things too

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require(`method-override`);
 var app = express();
+var Sequelize = require('sequelize');
+var env       = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/config/config.json')[env];
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 //requireing our models for syncing
 var db = require("./public/js");
 app.use(methodOverride('_method'));
