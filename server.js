95d10e874ac6e3b6985e49f9658cb38a5429e34c
travelingdoodle <reggiebrown@gmail.com>
2017-10-21 01:23:33 -0500

    updated npm and broke the sequelize

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require(`method-override`);
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -7,6 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//requireing our models for syncing
+var db = require("./public/js");
+app.use(methodOverride('_method'));
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -18,7 +23,7 @@ require("./routes/html-routes.js")(app);
 //   console.log("App listening on PORT " + PORT);
 // });
 // new comment
-sequelize.sync({force: true}).then(function(){
+db.sequelize.sync({force: true}).then(function(){
 	app.listen(PORT, function(){
 	console.log("listenning on http://localhost:" + PORT);
 });
